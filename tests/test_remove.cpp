c781429df7bc963d2f49aff91c879614a0146494
RobbieGM <rgm020103@gmail.com>
2025-01-27 17:00:28 -0500

    test_remove added (not passing)

---
+++ tests/test_remove.cpp
@@ -0,0 +1,30 @@
+#include "../diskmap.h"
+#include <assert.h>
+#include <cstdio>
+
+int main() {
+  remove("test.dm");
+  diskmap::DiskMap map("test.dm");
+  const char short_value[8]{};
+
+  map.write("111", static_cast<const void *>(short_value), sizeof(short_value));
+  map.write("dz", static_cast<const void *>(short_value), sizeof(short_value));
+  map.write("uJi", static_cast<const void *>(short_value), sizeof(short_value));
+  map.write("444", static_cast<const void *>(short_value), sizeof(short_value));
+
+  map.remove("dz");
+  map.remove("444");
+
+  bool found;
+  map.read("111", found);
+  assert(found);
+  map.read("dz", found);
+  assert(!found);
+  map.read("uJi", found);
+  assert(found);
+  map.read("444", found);
+  assert(!found);
+
+  map.debug_dump();
+  return 0;
+}
