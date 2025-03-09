55d4af6403b9a878521a6c8e1b00d06cb6ca96a5
RobbieGM <rgm020103@gmail.com>
2025-03-09 19:32:06 -0400

    test_stress

---
+++ tests/test_stress.cpp
@@ -0,0 +1,55 @@
+#include "../diskmap.h"
+#include <assert.h>
+#include <cstdio>
+#include <string>
+
+void add_600() {
+  diskmap::DiskMap map("test.dm");
+  const char value[3500]{};
+  for (int a = 0; a < 60; a++) {
+    auto tx = map.begin_rw_transaction();
+    for (int b = 0; b < 10; b++) {
+      int i = (a * 10) + b;
+      // NOLINTNEXTLINE(readability-redundant-string-cstr)
+      whl::string key = std::to_string(i).c_str();
+      tx.write(key, value, sizeof(value));
+    }
+    tx.commit();
+  }
+
+  printf("After writing 600 entries:\n");
+  {
+    auto tx = map.begin_ro_transaction();
+    tx.debug_dump();
+  }
+}
+
+void remove_600() {
+  diskmap::DiskMap map("test.dm");
+  for (int a = 0; a < 60; a++) {
+    auto tx = map.begin_rw_transaction();
+    for (int b = 0; b < 10; b++) {
+      int i = (a * 10) + b;
+      // NOLINTNEXTLINE(readability-redundant-string-cstr)
+      whl::string key = std::to_string(i).c_str();
+      if (!tx.remove(key)) {
+        printf("Warning: %d missing\n", i);
+      }
+    }
+    tx.commit();
+  }
+  printf("After deleting all entries:\n");
+  {
+    auto tx = map.begin_ro_transaction();
+    tx.debug_dump();
+  }
+}
+
+int main() {
+  remove("test.dm");
+  remove("test.dm.wal");
+  add_600();
+  remove_600();
+  add_600();
+  return 0;
+}
