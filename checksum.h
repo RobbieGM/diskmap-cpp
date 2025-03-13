3cf854ecce9cdfb54b82c503465ed0c9ea138df3
RobbieGM <rgm020103@gmail.com>
2025-03-13 15:06:18 -0400

    Checksum optimization

--- checksum.h
+++ checksum.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
+
 namespace diskmap {
 class Checksum {
   unsigned long state{1099511628211UL};
@@ -8,10 +10,18 @@ class Checksum {
 public:
   Checksum() = default;
   void digest(const char *data, size_t len) {
-    for (size_t i = 0; i < len; ++i) {
+    size_t i = 0;
+    while (i < (len / 8) * 8) {
+      state += *reinterpret_cast<const uint64_t *>(data + i);
+      state += (state << 10);
+      state ^= (state >> 6);
+      i += 8;
+    }
+    while (i < len) {
       state += data[i];
       state += (state << 10);
       state ^= (state >> 6);
+      i += 1;
     }
   }
   unsigned long value() const { return state; }
