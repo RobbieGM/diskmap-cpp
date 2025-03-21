eb3ed0c0c2891ae0e3489eefa0950d0f57de6cb1
RobbieGM <rgm020103@gmail.com>
2025-03-21 16:59:12 -0400

    Revert test_stress to 600

--- tests/test_stress.cpp
+++ tests/test_stress.cpp
@@ -8,8 +8,8 @@ void add_600() {
   const char value[3500]{};
   for (int a = 0; a < 60; a++) {
     auto tx = map.begin_rw_transaction();
-    for (int b = 0; b < 100; b++) {
-      int i = (a * 100) + b;
+    for (int b = 0; b < 10; b++) {
+      int i = (a * 10) + b;
       // NOLINTNEXTLINE(readability-redundant-string-cstr)
       whl::string key = std::to_string(i).c_str();
       tx.write(key, value, sizeof(value));
@@ -28,8 +28,8 @@ void remove_600() {
   diskmap::DiskMap map("test.dm");
   for (int a = 0; a < 60; a++) {
     auto tx = map.begin_rw_transaction();
-    for (int b = 0; b < 100; b++) {
-      int i = (a * 100) + b;
+    for (int b = 0; b < 10; b++) {
+      int i = (a * 10) + b;
       // NOLINTNEXTLINE(readability-redundant-string-cstr)
       whl::string key = std::to_string(i).c_str();
       if (!tx.remove(key)) {
